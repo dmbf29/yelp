540e696b025ea8ac71e5dff766e0fedfd0bab96d
Douglas Berkley <douglasmberkley@gmail.com>
2018-08-08 11:31:11 +0900

    yelp complete with reviews

---
+++ app/controllers/admin/restaurants_controller.rb
@@ -0,0 +1,6 @@
+class Admin::RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+    # @restaurants = current_user.restaurants
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    collection do
+      get 'top', to: 'restaurants#top', as: :top
+    end
+    member do
+      get 'chef', to: 'restaurants#chef', as: :chef
+    end
+
+    resources :reviews, only: [:new, :create]
+  end
+
+  # namespace :admin do
+  #   resources :restaurants, only: [:index]
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
